da7ee9faad13463a0e4a8e2d1596408448ce2db5
Elmer <elmer.huth@edu.huddinge.se>
2023-09-21 14:02:41 +0200

    Added two for loops that generate the deck

---
+++ script.js
@@ -0,0 +1,8 @@
+// First number is the value, second is the sleeve
+// [3, 2] = Three of Diamonds
+let deck = [];
+for (let i = 0; i < 4; i++){
+    for (let x = 0; x < 13; i++){
+        deck.push([x, i]);
+    }
+}
