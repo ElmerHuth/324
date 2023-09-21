5e222f811b6fe5b2d33d13c2c3b2cb6d66d42529
Elmer <elmer.huth@edu.huddinge.se>
2023-09-21 14:09:16 +0200

    added empty deal function and added a variable to keep track of all the players hands

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // First number is the value, second is the sleeve
 // [3, 2] = Three of Diamonds
+let playerhands = [];
 let deck = [];
 for (let i = 0; i < 4; i++){
     for (let x = 0; x < 13; i++){
         deck.push([x, i]);
     }
+}
+
+shuffle();
+
+function deal(){
+
+}
+
+function shuffle(){
+    // Add a random index of the deck to the end of temporary deck
+    // Remove that index from deck
+    // When the deck is empty, newdeck will be a shuffled deck
+    // assign decks value to be newdeck to finish the change
+    let newdeck = [];
+    while (newdeck.length < 52){
+        let randomindex = Math.floor(Math.random() * (deck.length - 1 + 1))
+        newdeck.push(deck[randomindex]);
+        deck.splice(randomindex, 1);
+    }
+    deck = newdeck;
 }
